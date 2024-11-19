b46483480706ce30603a4f6d0893bb3632f71a2f
acasta-yhliu <yhliu2000@outlook.com>
2024-11-19 16:32:16 -0500

    [update] implemented partial wrapper for chessIO and some test cases

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,6 @@
--- * Entry point for a Haskell application
+-- \* Entry point for a Haskell application
 -- Typically, this file is short and most code is part of a reusable library
 module Main where
 
-import Lib
-
--- >>> someDecl
-
 main :: IO ()
-main = putStrLn someDecl
+main = putStrLn "Hello World"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,62 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Board
+import Data.Ix (range)
+import Evaluate
 import Test.HUnit
 import Test.QuickCheck
 
-import Lib
+-- Tests for Board
 
--- >>> someDecl
+instance Arbitrary Color where
+  arbitrary = elements [Black, White]
+  shrink x = []
+
+instance Arbitrary Square where
+  arbitrary = elements $ range (minBound, maxBound)
+  shrink = enumFrom
+
+-- | Board with history
+data HistoryBoard = HB {unboard :: Position, history :: Maybe HistoryBoard}
+
+instance Show HistoryBoard where
+  show = show . unboard
+
+initialHB = HB initial Nothing
+
+instance Arbitrary HistoryBoard where
+  arbitrary :: Gen HistoryBoard
+  arbitrary = sized $ aux initialHB
+    where
+      aux hb@(HB b _) n =
+        if n == 0
+          then return hb
+          else
+            let m = moves b
+             in if null m
+                  then return hb
+                  else do
+                    next <- elements m
+                    aux (HB (enginePlay next b) (Just hb)) (n - 1)
+
+  shrink hb = case history hb of
+    Nothing -> []
+    Just h -> [h]
+
+prop_parseSquareInvariant :: Square -> Bool
+prop_parseSquareInvariant s = parseSquare (show s) == Just s
+
+prop_boardColorFlip :: HistoryBoard -> Bool
+prop_boardColorFlip (HB b _) = all (\m -> player b /= player (enginePlay m b)) (moves b)
+
+-- Tests for Evaluate
+
+return []
+
+runTests = $quickCheckAll
 
 main :: IO ()
-main = do 
-    putStrLn someDecl
-    putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn "\n=====Tests==================================="
+  _ <- runTests
+  putStrLn "=====Tests==================================="
