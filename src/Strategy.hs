5db8bc82753a19be97710b9ec89d7f4d239f3a4d
acasta-yhliu <yhliu2000@outlook.com>
2024-11-20 15:02:56 -0500

    [update] added evaluate move and best move

--- src/Strategy.hs
+++ src/Strategy.hs
@@ -6,11 +6,25 @@ module Strategy
   )
 where
 
+import Data.Foldable (maximumBy)
+import Data.Function ((&))
 import Data.Kind (Type)
-import Game (Game)
+import Data.Ord (comparing)
+import Game (Game (..))
 
 class (Game a, Ord (Score a)) => Evaluate a where
   type Score a :: Type
 
   -- | evaluate the current situation, larger score means better and smaller score means worse
   evaluate :: a -> Score a
+
+  -- | evaluate a move under the current situation
+  evaluateMove :: a -> Move a -> Score a
+  evaluateMove c m = evaluate (play c m)
+
+class (Evaluate a) => Strategy a where
+  -- | return the best move if there is one
+  bestMove :: a -> Maybe (Move a)
+  bestMove s = case moves s of
+    [] -> Nothing
+    x -> x & map (\m -> (evaluateMove s m, m)) & maximumBy (comparing fst) & Just . snd
